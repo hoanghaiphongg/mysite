dd02ad844fa67ac2fdf33836484977c60306a83b
hoanghaiphongg <hoanghaiphongngu2@gmail.com>
2024-07-22 18:08:31 +0700

    Django Tutorial Part 3: Using models

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,24 +2,32 @@ from django.db import models
 from django.urls import reverse
 import uuid
 
-
 # Create your models here.
 class Genre(models.Model):
-    name = models.CharField(max_length=200, help_text='Enter the genre')
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g.Science Fiction)')
+    
     def __str__(self):
+        """String for representing the Model object."""
         return self.name
-
+    
 class Book(models.Model):
+    """Models representing a book."""
     title = models.CharField(max_length=200)
-    author = models.CharField('Author',on_delete=models.SET_NULL,null=True)
-    sumary = models.TextField(max_length=100, help_text='Enter a brief description')
-    isbn = models.CharField('ISBN',max_length=13,unique=True,help_text='13 Character <ahref="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField("ISBN", max_length=13, unique=True, help_text='13 Character <ahref="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
     def __str__(self):
+        """String for representing the Model object."""
         return self.title
     
     def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
     
 class BookInstance(models.Model):
