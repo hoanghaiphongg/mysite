c55abeaf1e3d88f13fae51b8f5f21f0b21580770
hoanghaiphongg <hoanghaiphongngu2@gmail.com>
2024-07-22 18:12:03 +0700

    Django Tutorial Part 3: Using models

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 import uuid
 
-# Create your models here.
 class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g.Science Fiction)')
@@ -74,3 +73,4 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f"{self.last_name}, {self.first_name}"
+    
