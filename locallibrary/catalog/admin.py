f7f6ecde251300b4adbae984db18ac043b3e584f
Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
2024-07-22 16:28:01 +0700

    Django Tutorial Part 5: Creating our home page
    commit 9942ce9c5262b28b1ba73fbb0c9eff5d2abe30bd
    Merge: c4a1a1e 738ed8d
    Author: Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
    Date:   Mon Jul 29 18:47:47 2024 +0700
    
        Merge pull request #5 from hoanghaiphongg/part3.1
    
        Django Tutorial Part 3: Using models
    
    commit 738ed8df865c073521fa9252234abe9c6f41a331
    Merge: ef361dc c4a1a1e
    Author: Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
    Date:   Mon Jul 29 18:47:18 2024 +0700
    
        Merge branch 'main' into part3.1
    
    commit ef361dc803b930d6a640c926d7b7b9b14a57f9ff
    Author: Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
    Date:   Mon Jul 22 16:28:26 2024 +0700
    
        Django Tutorial Part 3: Using models
    
    commit c4a1a1ef5e5b6c15b0e82a48c7e87378cdedd40f
    Merge: 233b790 f08d0f6
    Author: Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
    Date:   Fri Jul 26 13:30:17 2024 +0700
    
        Merge pull request #3 from hoanghaiphongg/part2
    
        Django Tutorial Part 2: Creating a skeleton website
    
    commit f08d0f6c0cde29d2f475ddb5c0181b09ecd81ae8
    Author: Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
    Date:   Mon Jul 22 16:28:26 2024 +0700
    
        Django Tutorial Part 2: Creating a skeleton website
    
    commit 233b79020f51d40f4f3c448adb449dd49755ce0d
    Author: Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
    Date:   Mon Jul 22 16:28:26 2024 +0700
    
        Update README.md
    
    commit de87966b0336f43d2e2bd69f589ac53e18d61c9a
    Author: Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
    Date:   Mon Jul 22 16:28:01 2024 +0700
    
        Update README.md

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+#Another way
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
